d3a179377bd88d527d726bbd7a76c2381f796d87
Jimena Royo-Letelier <jimena@melix.net>
2018-09-08 23:41:10 +0200

    removed mgos old test

--- src/micro/dev/build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20180603-185826/???";
-const char *build_timestamp = "2018-06-03T18:58:26Z";
-const char *build_version = "1.0";
--- src/micro/dev/build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20180603-185826/???";
-const char *mg_build_timestamp = "2018-06-03T18:58:26Z";
-const char *mg_build_version = "2018060318";
