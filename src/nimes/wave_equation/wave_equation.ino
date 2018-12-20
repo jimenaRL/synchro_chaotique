498dcccdfba299e67176adef207720ae85730140
jimenarl <jimena@melix.net>
2018-12-20 14:25:13 +0100

    Just to transort

--- src/nimes/wave_equation/wave_equation.ino
+++ src/nimes/wave_equation/wave_equation.ino
@@ -12,9 +12,9 @@
 #define CONTROL_RATE 4 // in Hz, must be a power of 2 
 
 /// debug ///
-const int PPFPRE = 7; // print precision 
+const int PPFPRE = 5; // print precision 
 int BAUD_RATE = 9600;
-bool DEBUG = false;
+bool DEBUG = true;
 bool MONITOR = true;
 
 // WiFi - UDP
@@ -22,27 +22,30 @@ const char* SSID = "esthetopies";
 const char* PWD = "esthetopies";
 
 WiFiUDP Udp;
-const unsigned int commPort = 8266;   // local port to listen for UDP packets
-const int intIp = 11; // IP para este dispositivo sera ip(192, 168, 0, intIp);
-const IPAddress gateway(192, 168, 0, 1);  // IP gateway, normalmente es la del router
+const unsigned int COMMPORT = 8266;  // local port to listen for UDP packets
+const int IP0 = 192;
+const int IP1 = 168;
+const int IP2 = 0;
+const int selfIdIp = 10; // IP para este dispositivo sera ip(192, 168, 0, idIp);
+const IPAddress gateway(IP0, IP1, IP2, 1);  // IP gateway, normalmente es la del router
 const IPAddress subnet(255, 255, 255, 0); // Mascara de subred
 OSCErrorCode error;
 
 // model
-const int NB_NEIGHS = 6; // includes self
-float in_value;
+const int NB_NEIGHS = 5;
 
 typedef struct{
-    int intIp;
+    int idIp;
     float current_val;
     float previous_val;
     float weight;
 } Node;
 
 const float self_weight = 0.0;
-// ring geometry
-Node Neighs[6]={
- {10, 0.0, 0.0, 1.0},
+Node Self = {selfIdIp, 0.0, 0.0, self_weight};
+
+Node Neighs[5]={
+ {11, 0.0, 0.0, 2.0},
  {12, 0.0, 0.0, 0.0},
  {13, 0.0, 0.0, 0.0},
  {14, 0.0, 0.0, 0.0},
@@ -54,9 +57,7 @@ int NB_MONITORS = 3;
 int Monitors[3] = {111, 112, 113};
 
 float C = 0.051;  // sound speed
-float MU = 0.0;  // viscosity
-float current_val = 10.0;  // internal value
-float previous_val = 10.0;  // internal previous value
+float MU = 0.01;  // viscosity
 
 // Mozzi Synth
 
@@ -70,7 +71,7 @@ Oscil<COS8192_NUM_CELLS, AUDIO_RATE> aCos0(COS8192_DATA);
 
 void connect_wifi(){
   Serial.printf("\nConnecting to %s ", SSID);
-  WiFi.config(IPAddress(192, 168, 0, intIp), gateway, subnet);
+  WiFi.config(IPAddress(IP0, IP1, IP2, Self.idIp), gateway, subnet);
   WiFi.begin(SSID, PWD);
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
@@ -83,53 +84,21 @@ void connect_wifi(){
 
 void connect_udp(){
   Serial.println("Starting UDP");
-  Udp.begin(commPort);
+  Udp.begin(COMMPORT);
   Serial.print("Local port: ");
   Serial.println(Udp.localPort());
 }
 
-void show_params(){
-    Serial.println("Kuramoto model parameters:");
-    Serial.printf("\t C: ");
-    Serial.println(C, PPFPRE);
-    Serial.printf("\t MU: ");
-    Serial.println(MU, PPFPRE);
-    Serial.printf("\t DEBUG: ");
-    Serial.println(DEBUG);
-    Serial.printf("\t MONITOR: ");
-    Serial.println(MONITOR);
-}
-
-void show_neighs(){
-  Serial.print("Nodes:\n");
-  for (int i=0; i<NB_NEIGHS; i++){
-   if(Neighs[i].weight > 0){
-     Serial.printf("\tintIp %d | weight ", Neighs[i].intIp);
-     Serial.print(Neighs[i].weight, PPFPRE);
-     Serial.print(" | current_val "),
-     Serial.println(Neighs[i].current_val, PPFPRE);
-   }
-  }
-}
-
-void show_this(){
-  Serial.printf("\tintIp %d | ---------- | current_val ", intIp);
-  Serial.print(current_val, PPFPRE);
-  Serial.printf("| previous_val ", intIp);
-  Serial.print(previous_val, PPFPRE);
-  Serial.println("");
-}
-
 void monitor(){
   OSCMessage msg("/monitor");
-  msg.add(current_val);
+  msg.add(Self.current_val);
   if(DEBUG){
     Serial.print("Sending value ");
-    Serial.print((float)(current_val), PPFPRE);
+    Serial.print((float)(Self.current_val), PPFPRE);
     Serial.print(" to monitors:");
   }
   for (int i=0; i<NB_MONITORS; i++){
-    Udp.beginPacket(IPAddress(192, 168, 0, Monitors[i]), MONITOR_PORT);
+    Udp.beginPacket(IPAddress(IP0, IP1, IP2, Monitors[i]), MONITOR_PORT);
     msg.send(Udp);
     Udp.endPacket();
     if(DEBUG){
@@ -144,8 +113,55 @@ void monitor(){
 }
 
 
-/// callbacks ///
+/// showers ///
 
+void show_params(){
+    Serial.println("Model parameters:");
+    Serial.printf("\tC: ");
+    Serial.println(C, PPFPRE);
+    Serial.printf("\tMU: ");
+    Serial.println(MU, PPFPRE);
+    Serial.printf("\tDEBUG: ");
+    Serial.println(DEBUG);
+    Serial.printf("\tMONITOR: ");
+    Serial.println(MONITOR);
+}
+
+void show_neighs(){
+  Serial.print("Neighs:\n");
+  for (int i=0; i<NB_NEIGHS; i++){
+   if(Neighs[i].weight > 0){
+      Serial.printf("\tidIp %d | weight ", Neighs[i].idIp);
+      Serial.print(Neighs[i].weight, PPFPRE);
+      Serial.print(" | current_val "),
+      Serial.print(Neighs[i].current_val, PPFPRE);
+      Serial.print(" | previous_val ");
+      Serial.println(Self.previous_val, PPFPRE);
+   }
+  }
+}
+
+void show_this(){
+  Serial.print("Self:\n");
+  Serial.printf("\tidIp %d | weight ------- | current_val ", Self.idIp);
+  Serial.print(Self.current_val, PPFPRE);
+  Serial.print(" | previous_val ");
+  Serial.println(Self.previous_val, PPFPRE);
+}
+
+void show_all(){
+  show_params();
+  show_this();
+  show_neighs();
+}
+
+void show_params_ckb(OSCMessage &msg){
+  show_params();
+}
+
+void show_all_ckb(OSCMessage &msg){
+  show_all();
+}
 void show_this_ckb(OSCMessage &msg){
   show_this();
 }
@@ -153,6 +169,8 @@ void show_this_ckb(OSCMessage &msg){
 void show_neighs_ckb(OSCMessage &msg){
   show_neighs();
 }
+
+/// setters ///
 
 void set_DEBUG(OSCMessage &msg){
   DEBUG = (bool) msg.getInt(0);
@@ -165,44 +183,6 @@ void set_MONITOR(OSCMessage &msg){
   if(DEBUG){
     Serial.print("MONITOR ");
     Serial.println(MONITOR);
-  }
-}
-
-void current_val_ckb(OSCMessage &msg){
-  current_val = msg.getFloat(0);
-  if(DEBUG){
-    Serial.print("Current value updated with value ");
-    Serial.print(current_val, PPFPRE);
-    Serial.println("");
-  }
-}
-
-void set_neighbors(OSCMessage &msg) {
-  int intIp_ = msg.getInt(0);
-  for (int i=0; i<NB_NEIGHS; i++){
-    if(Neighs[i].intIp == intIp_){
-      Neighs[i].current_val = msg.getFloat(1);
-      Neighs[i].previous_val = msg.getFloat(2);
-      if(DEBUG){
-        Serial.print("Neighbor ");
-        Serial.print(Neighs[i].intIp);
-        Serial.print(" current and previous values updated with values: ");
-        Serial.print(Neighs[i].current_val, PPFPRE);
-        Serial.print(" and ");
-        Serial.println(Neighs[i].previous_val, PPFPRE);
-      }
-    }
-  }
-}
-
-void set_values(OSCMessage &msg) {
-  current_val = msg.getFloat(0);
-  previous_val = msg.getFloat(1);
-  if(DEBUG){
-    Serial.print("Current and previous values updated with values: ");
-    Serial.print(current_val, PPFPRE);
-    Serial.print(" and ");
-    Serial.println(previous_val, PPFPRE);
   }
 }
 
@@ -239,68 +219,101 @@ void set_freq(OSCMessage &msg){
   }
 }
 
-void neighbor_weight(OSCMessage &msg){
-  int idx = msg.getInt(0);
+void set_weights(OSCMessage &msg){
+  int in_idIp = msg.getInt(0);
   for (int i=0; i<NB_NEIGHS; i++){
-    if(Neighs[i].intIp == idx){
+    if(Neighs[i].idIp == in_idIp){
       Neighs[i].weight = msg.getFloat(1);
       if(DEBUG){
-        Serial.printf("Updated neighbor %d with weight ", idx);
-        Serial.println(Neighs[idx].weight);
+        Serial.printf("Updated neighbor %d with weight ", Neighs[i].idIp);
+        Serial.println(Neighs[i].weight);
       }
     }
   }
 }
+
+void set_values(OSCMessage &msg) {
+  int in_idIp = msg.getInt(0);
+  if(in_idIp == Self.idIp){
+      Self.current_val = msg.getFloat(1);
+      Self.previous_val = msg.getFloat(2);
+      if(DEBUG){
+        Serial.print("Self ");
+        Serial.print(Self.idIp);
+        Serial.print(" current and previous values updated with values: ");
+        Serial.print(Self.current_val, PPFPRE);
+        Serial.print(" and ");
+        Serial.println(Self.previous_val, PPFPRE);
+      }
+  }
+  else{
+    for (int i=0; i<NB_NEIGHS; i++){
+      if(Neighs[i].idIp == in_idIp){
+        Neighs[i].current_val = msg.getFloat(1);
+        Neighs[i].previous_val = msg.getFloat(2);
+        if(DEBUG){
+          Serial.print("Neighbor ");
+          Serial.print(Neighs[i].idIp);
+          Serial.print(" current and previous values updated with values: ");
+          Serial.print(Neighs[i].current_val, PPFPRE);
+          Serial.print(" and ");
+          Serial.println(Neighs[i].previous_val, PPFPRE);
+        }
+      }
+    }
+  }
+}
+
 
 // main 
 
 // update internal value using discrete wave equation
 void update_value(){
 
-  float D2TP = 2.0 * current_val - previous_val;
+  float D2TP = 2.0 * Self.current_val - Self.previous_val;
   float D2XP_current = 0.0;
   float D2XP_previous = 0.0;
   int NB_ACT_NEIGHS = 0;
   for(int j=0; j<NB_NEIGHS; j++){
     if(Neighs[j].weight > 0){
-      D2XP_previous += (Neighs[j].previous_val - previous_val);
-      D2XP_current += (Neighs[j].current_val - current_val);
-      Serial.print(j);
-      Serial.print(" : D2XP_current ");
-      Serial.print(D2XP_current, PPFPRE);
-      Serial.println("");
+      D2XP_previous += (Neighs[j].previous_val - Self.previous_val);
+      D2XP_current += (Neighs[j].current_val - Self.current_val);
+      //Serial.print(j);
+      //Serial.print(" : D2XP_current ");
+      //Serial.print(D2XP_current, PPFPRE);
+      //Serial.println("");
     }
   }
-  previous_val = current_val;
+  Self.previous_val = Self.current_val;
   // DTD2XP_current = (D2XP_current - D2XP_previous)
-  current_val = D2TP + C * D2XP_current + MU * (D2XP_current - D2XP_previous);
-  Serial.print("D2TP ");
-  Serial.print(D2TP, PPFPRE);
-  Serial.println("");
+  Self.current_val = D2TP + C * D2XP_current + MU * (D2XP_current - D2XP_previous);
+  //Serial.print("D2TP ");
+  //Serial.print(D2TP, PPFPRE);
+  //Serial.println("");
   if(DEBUG){
     Serial.printf("New internal value ");
-    Serial.print(current_val, PPFPRE);
+    Serial.print(Self.current_val, PPFPRE);
     Serial.println(".");
   }
 }
 
 // send internal value to neighbors
-void send_message(){
-  OSCMessage msg("/neighbor");
-  msg.add(current_val);
+void send_value(){
+  OSCMessage msg("/value");
+  msg.add(Self.current_val);
   if(DEBUG){
     Serial.print("Sending value ");
-    Serial.print(current_val, PPFPRE);
+    Serial.print(Self.current_val, PPFPRE);
     Serial.print(" to neighbors:");
   }
   for (int i=0; i<NB_NEIGHS; i++){
     if(Neighs[i].weight > 0){
-      Udp.beginPacket(IPAddress(192, 168, 0, Neighs[i].intIp), commPort);
+      Udp.beginPacket(IPAddress(IP0, IP1, IP2, Neighs[i].idIp), COMMPORT);
       msg.send(Udp);
       Udp.endPacket();
       if(DEBUG){
         Serial.print(" ");
-        Serial.print(Neighs[i].intIp);
+        Serial.print(Neighs[i].idIp);
       }
     }
   }
@@ -311,14 +324,15 @@ void send_message(){
 }
 
 // receive value from neighbor
-void neighbor(OSCMessage &msg){
-  in_value = msg.getFloat(0);
+void receive_value(OSCMessage &msg){
+  float in_value = msg.getFloat(0);
   // update neighbors
   for (int i=0; i<NB_NEIGHS; i++){
-    if(Udp.remoteIP()[3] == Neighs[i].intIp && Neighs[i].weight > 0){
-      Neighs[i] = (Node){Neighs[i].intIp, in_value, current_val, Neighs[i].weight};
+    if(Udp.remoteIP()[3] == Neighs[i].idIp && Neighs[i].weight > 0){
+      Neighs[i].previous_val = Neighs[i].current_val;
+      Neighs[i].current_val = in_value;
       if(DEBUG){
-        Serial.printf("Updating neighbor %d with value ", Neighs[i].intIp);
+        Serial.printf("Updating neighbor %d with value ", Neighs[i].idIp);
         Serial.println(in_value, PPFPRE);
       }
     }
@@ -326,15 +340,14 @@ void neighbor(OSCMessage &msg){
   // update internal value
   update_value();
   // send internal value to neighbors
-  send_message();
-  // send internal value to monitors
+  send_value();
   if(MONITOR){
     monitor();
   }
 }
 
 
-void parse_message(){
+void parse_osc(){
   OSCMessage msg;
   int size = Udp.parsePacket();
   if (size > 0){
@@ -342,19 +355,22 @@ void parse_message(){
       msg.fill(Udp.read());
     }
     if (!msg.hasError()){
-      msg.dispatch("/show_this", show_this_ckb);
-      msg.dispatch("/show_neighs", show_neighs_ckb);
-      msg.dispatch("/current_val", current_val_ckb);
-      msg.dispatch("/set_neighbors", set_neighbors);
-      msg.dispatch("/set_values", set_values);
-      msg.dispatch("/param/C", set_C);
-      msg.dispatch("/param/MU", set_MU);
-      msg.dispatch("/DEBUG", set_DEBUG);
-      msg.dispatch("/MONITOR", set_MONITOR);
-      msg.dispatch("/oscillator/freq", set_freq);
-      msg.dispatch("/oscillator/amp", set_amp);
-      msg.dispatch("/neighbor", neighbor);
-      msg.dispatch("/neighbor_weight", neighbor_weight);
+      msg.dispatch("/show/all", show_all_ckb);
+      msg.dispatch("/show/this", show_this_ckb);
+      msg.dispatch("/show/neighs", show_neighs_ckb);
+      msg.dispatch("/show/params", show_params_ckb);
+
+      msg.dispatch("/set/weights", set_weights);  // to check
+      msg.dispatch("/set/values", set_values);
+
+      msg.dispatch("/set/C", set_C);
+      msg.dispatch("/set/MU", set_MU);
+      msg.dispatch("/set/DEBUG", set_DEBUG);
+      msg.dispatch("/set/MONITOR", set_MONITOR);
+      msg.dispatch("/set/oscFreq", set_freq);
+      msg.dispatch("/set/oscAmp", set_amp);
+
+      msg.dispatch("/value", receive_value); // to check
     } else{
       error = msg.getError();
       Serial.print("Message error: ");
@@ -363,13 +379,13 @@ void parse_message(){
   }
 }
 
-// This runs CONTROL_RATE times per second.
+// This runs CONTROL_RATE times per second
 void updateControl(){
-  parse_message();
-  aCos0.setFreq((int) 100*current_val);
+  parse_osc();
+  aCos0.setFreq((int) 100*Self.current_val);
 }
 
-// This runs on average 16384 times per second, so code here needs to be lean.
+// This runs on average 16384 times per second
 int updateAudio(){
   return  a0*aCos0.next();
 }
@@ -383,12 +399,7 @@ void setup(){
   connect_wifi();
   connect_udp();
 
-  show_this();
-  show_neighs();
-  show_params();
-
-  // set initial frequencies
-  aCos0.setFreq(500);
+  show_all();
 
   // set initial amplitude
   a0=32;
