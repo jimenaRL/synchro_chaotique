41b12c28b009339e8e761531878f7e1716ad5fe7
jimenarl <jimena@melix.net>
2018-12-21 15:48:29 +0100

    Add initialisation

--- src/nimes/master/master.py
+++ src/nimes/master/master.py
@@ -5,6 +5,8 @@ import random
 import argparse
 from copy import deepcopy
 
+NB_NODES = 33
+INIT = '0: 0.0,1.0; 1: 0.0,1.0; 2: 0.0,1.0; 3: 0.0,1.0; 4: 0.0,1.0; 5: 0.0,1.0; 6: 0.0,1.0; 7: 0.0,1.0; 8: 0.0,1.0; 9: 0.0,1.0; 10: 0.0,1.0; 11: 0.0,1.0; 12: 0.0,1.0; 13: 0.0,1.0; 14:0.0,1.0; 15: 0.0,1.0; 16: 0.0,1.0; 17: 0.0,1.0; 18: 0.0,1.0; 19: 0.0,1.0; 20: 0.0,1.0; 21: 0.0,1.0; 22: 0.0,1.0; 23: 0.0,1.0; 24: 0.0,1.0; 25: 0.0,1.0; 26: 0.0,1.0; 27: 0.0,1.0;28: 0.0,1.0; 29: 0.0,1.0; 30: 0.0,1.0; 31: 0.0,1.0; 32: 0.0,1.0'
 
 class Node(object):
 
@@ -58,14 +60,16 @@ parser.add_argument('-steps',
 parser.add_argument('-init',
                     dest="init",
                     type=str,
-                    default=" ".join(["-1"]*33))
-
+                    default=INIT)
 
 for k, v in parser.parse_args().__dict__.items():
     locals()[k] = v
     print("%s: %s" % (k,v))
 
-init = map(float, init.split(" "))
+
+init = {int(s.split(":")[0]):
+(float(s.split(":")[1].split(",")[0]), float(s.split(":")[1].split(",")[1]))
+for s in init.split(";")}
 
 # ip - vertex table
 IP_VERTEX = [
@@ -104,7 +108,6 @@ IP_VERTEX = [
     (42, [5, -1, 2]),
 ]
 
-NB_NODES = len(IP_VERTEX)
 
 def get_ip(vertex):
     for ip, v in IP_VERTEX:
